d490d9ef97019f8642e13375f1f28cac1c441355
Chrishart <chrishart.estrada@gmail.com>
2024-01-13 08:30:15 +0800

    useState Hook

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 const NewPost = () => {
+  const [enteredBody, setEnteredBody] = useState('');
+
+  function changeBodyHandler(event){
+    setEnteredBody(event.target.value);
+  }
+
   return (
     <form>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3}></textarea>
+            <textarea id="body" required rows={3} onChange={changeBodyHandler}></textarea>
         </p>
+        <p>{enteredBody}</p>
         <p>
             <label htmlFor="name">Name</label>
             <input type="text" id="name" required />
