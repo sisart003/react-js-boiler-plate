2e072022ffe57be78adc1bcfdc5cc6762748d308
Chrishart <chrishart.estrada@gmail.com>
2024-01-13 09:20:35 +0800

    Insert Modal Functionality

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
 
 const PostsList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true); 
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event){
     setEnteredBody(event.target.value);
@@ -15,9 +21,23 @@ const PostsList = () => {
     setEnteredAuthor(event.target.value);
   }
 
+  // let modalContent;
+
+  // if(modalIsVisible){
+  //     modalContent = (
+  //       <Modal onClose={hideModalHandler}>
+  //         <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+  //       </Modal>
+  //     )
+  // }
+
   return (
-    <>
-        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+    <>  
+        {modalIsVisible && (
+          <Modal onClose={hideModalHandler}>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+          </Modal>
+        )}
         <ul>
             <Post author={enteredAuthor} body={enteredBody} />
         </ul>
