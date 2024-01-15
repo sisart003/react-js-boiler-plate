0a1fcf040c2daf3b6d0f9dc87faa2541c2ce1ec7
Chrishart <chrishart.estrada@gmail.com>
2024-01-15 11:03:58 +0800

    Dynamically adding Post DOM

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,14 +1,40 @@
-const NewPost = (props) => {
-  
+import { useState } from 'react';
+
+const NewPost = ({onCancel, onAddPost}) => {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event){
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event){
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event){
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor
+    };
+    onAddPost(postData);
+    onCancel(); 
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} onChange={props.onBodyChange}></textarea>
+            <textarea id="body" required rows={3} onChange={bodyChangeHandler}></textarea>
         </p>
         <p>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" required onChange={props.onAuthorChange} />
+            <input type="text" id="name" required onChange={authorChangeHandler} />
+        </p>
+        <p>
+          <button type='button' onClick={onCancel}>Cancel</button>
+          <button>Submit</button>
         </p>
     </form>
   )
